0a91a6a25c5bd115da905d7e4104086a803823fc
Viktor Dremsizov <viktorkz91@gmail.com>
2024-01-20 19:48:12 +0200

    my homework

---
+++ 04_Data Aggregation/Data_Aggregatopm_my_homework.sql
@@ -0,0 +1,132 @@
+
+															-- EX DATA Aggregation
+
+						-- 1. Records' Count
+
+use gringotts;
+
+-- С функцията COUNT избираме да ни преброи записаните файлове в дадената таблица. 
+-- При * избираме всички, можем да сложим и нещо специфично (first_name)
+SELECT COUNT(*) FROM wizzard_deposits;
+
+
+					-- 02. Longest Magic Wand
+          
+          -- С опцията на МАХ сравняваме всички стойности в графата magic_wand_size, след което с AS просто си даваме име на новата таблица
+	SELECT MAX(magic_wand_size) AS 'longest_magic_wand'
+    FROM wizzard_deposits;
+
+
+					-- 03. Longest Magic Wand per Deposit Groups
+
+
+SELECT deposit_group,
+MAX(magic_wand_size) AS 'longest_wand_size'
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY longest_wand_size ASC, deposit_group ASC;
+
+					-- 4. Smallest Deposit Group Per Magic Wand Size*
+
+SELECT deposit_group
+FROM wizzard_deposits
+GROUP BY deposit_group
+LIMIT 1;
+
+
+					-- 05. Deposits Sum
+
+-- Със финкцията СУМ избираме да сумираме.
+
+SELECT deposit_group,
+SUM(deposit_amount) AS 'total_sum'
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY total_sum ASC;
+
+					-- 06. Deposits Sum for Ollivander Family
+
+-- Тук с  WHERE си поставяме условието за това кой е правил магията. Трябва да се внимава за мястото на FROM.
+
+
+SELECT deposit_group,
+SUM(deposit_amount) AS 'total_sum'
+FROM wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+ORDER BY deposit_group ASC;
+
+					-- 07. Deposits Filter
+                    
+
+SELECT deposit_group,
+SUM(deposit_amount) AS 'total_sum'
+FROM wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family' 
+GROUP BY deposit_group
+HAVING total_sum < 150000
+ORDER BY total_sum DESC;              
+	
+					-- 08. Deposit Charge
+    
+SELECT deposit_group, magic_wand_creator,
+MIN(deposit_charge) AS min_deposit_charge
+FROM wizzard_deposits
+GROUP BY deposit_group, magic_wand_creator
+ORDER BY magic_wand_creator ASC, deposit_group ASC;
+
+
+					-- 09. Age Groups
+                    
+		
+                    
+SELECT
+  CASE
+    WHEN age BETWEEN 0 AND 10 THEN '[0-10]'
+    WHEN age BETWEEN 11 AND 20 THEN '[11-20]'
+    WHEN age BETWEEN 21 AND 30 THEN '[21-30]'
+    WHEN age BETWEEN 31 AND 40 THEN '[31-40]'
+    WHEN age BETWEEN 41 AND 50 THEN '[41-50]'
+    WHEN age BETWEEN 51 AND 60 THEN '[51-60]'
+    ELSE '[61+]'
+  END AS age_group,
+  COUNT(*) AS wizard_count
+FROM wizzard_deposits
+GROUP BY age_group
+ORDER BY MIN(age) ASC;
+
+
+					-- 10. First Letter
+
+SELECT LEFT(first_name, 1) as first_letter
+FROM wizzard_deposits
+WHERE deposit_group = 'Troll CHest'
+GROUP BY first_letter
+ORDER BY first_letter ASC;
+
+
+					-- 11. Average Interest
+                 
+ SELECT deposit_group,is_deposit_expired, AVG(deposit_interest) AS 'average_interest'
+ FROM wizzard_deposits
+ WHERE deposit_start_date > '1985-01-01'
+ GROUP BY deposit_group,is_deposit_expired
+ ORDER BY deposit_group DESC, is_deposit_expired ASC;
+ 
+ 
+			-- 12. Employees Minimum Salaries
+            
+use soft_uni;
+
+SELECT department_id,
+	MIN(salary) AS 'minimum_salary'
+FROM employees
+WHERE department_id IN(2,5,7)
+AND hire_date > '2000-01-01'
+GROUP BY department_id
+ORDER BY department_id ASC;
+
+
+
+		
+
